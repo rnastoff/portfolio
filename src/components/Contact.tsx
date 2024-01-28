ca06440bdb388614af863ac4efbd242e757af4d6
Rob Nastoff <robnastoff@gmail.com>
2024-01-27 18:51:50 -0600

    contact error handling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,49 +6,62 @@ import SectionHeader from "./SectionHeader";
 import SectionBodyWrapper from "./SectionBodyWrapper";
 
 export default function Contact() {
-  const [status, setStatus] = useState();
-  //please fill out all fields
-  //There was an error
-  //Success! Message sent.
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [textArea, setTextArea] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState("");
 
   const form = useRef<HTMLFormElement>(null);
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-
     const currentForm = form.current;
-    console.log("FORM:", form);
-    // this prevents sending emails if there is no form.
-    // in case currentForm cannot possibly ever be null,
-    // you could alert the user or throw an Error, here
+
+    console.log("FORM: ", form.current);
+
     if (currentForm == null) {
-      console.log("not sending");
+      // This is necessary for typescript
+      return;
+    }
+
+    if (!name || !email || !textArea) {
+      setSuccess(false);
+      setError("Please fill out all fields");
       return;
     }
 
     emailjs.sendForm(serviceId, templateId, currentForm, publicKey).then(
       (result) => {
-        console.log("success");
+        setError("");
+        setSuccess(true);
         currentForm.reset();
-        // e.target.reset();
       },
       (error) => {
-        console.log("error");
-        console.log(error.txt);
+        console.log("send error");
+        setSuccess(false);
+        setError("Something went wrong!");
       }
     );
   }
+
+  const errorMessage = <p className=" text-[#a82727] text-sm text-center mb-4">{error}</p>;
+  const successMessage = <p className=" text-[#47bd2f] text-sm text-center mb-4">Success! Message sent.</p>;
 
   return (
     <section className="mt-8">
       <SectionHeader title="CONTACT" />
       <SectionBodyWrapper>
         <form ref={form} onSubmit={handleSubmit} className="flex flex-col py-3 px-2">
+          {error && errorMessage}
+          {success && successMessage}
           <input
-            className="w-full bg-[#191919] bg-opacity-50 text-[#eeeeee] border border-[#3a3a3a] text-sm p-2 outline-none placeholder-[#aaaaaa]"
+            className="w-full bg-[#191919] bg-opacity-50 text-[#eeeeee] border border-[#3a3a3a] text-sm p-2 outline-none placeholder-[#aaaaaa] "
             type="text"
             placeholder="Name"
             name="user_name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
             required
           />
           <input
@@ -56,12 +69,16 @@ export default function Contact() {
             type="email"
             placeholder="Email"
             name="user_email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             required
           />
           <textarea
             className="w-full h-32 bg-[#191919] bg-opacity-50 text-[#eeeeee] border border-[#3a3a3a] text-sm p-2 outline-none placeholder-[#aaaaaa] resize-none mt-4"
             placeholder="Send me a message"
             name="message"
+            onChange={(e) => setTextArea(e.target.value)}
+            value={textArea}
             required
           />
           <div className="flex ">
