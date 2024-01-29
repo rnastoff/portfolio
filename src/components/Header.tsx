d70d3dc719db7e52c1ee234d51c6186c0a13d3af
Rob Nastoff <robnastoff@gmail.com>
2024-01-28 19:04:14 -0600

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex justify-between sm:mt-8 mt-2">
       <div className="self-end">
         <h1 className="text-[#eeeeee] sm:text-5xl text-3xl font-black">ROB NASTOFF</h1>
-        <h2 className="text-[#aaaaaa] sm:text-xl text-lg sm:-mt-1 -mt-2">REACT DEVELOPER</h2>
+        <h2 className="text-[#aaaaaa] sm:text-xl text-lg sm:-mt-1 -mt-2 ml-[1px]">REACT DEVELOPER</h2>
       </div>
       <div className="self-end">
         <div className="flex justify-end">
